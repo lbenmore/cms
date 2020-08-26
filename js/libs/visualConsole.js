836a80cc285a089d479c49902131f1c8bd2140ec
Lisa Benmore <lbenmore@gmail.com>
2020-08-26 16:35:05 -0700

    added drag functionality and a clear button

--- js/libs/visualConsole.js
+++ js/libs/visualConsole.js
@@ -1,11 +1,12 @@
 const output = document.createElement('div');
+const btnClear = document.createElement('button');
 const _log = console.log;
 
-output.setAttribute('contenteditable', 'true');
 Object.assign(output.style, {
   position: 'fixed',
   top: '8px',
-  left: `${innerWidth - (innerWidth / 4) - 8}px`,
+  // left: `${innerWidth - (innerWidth / 4) - 8}px`,
+  left: '8px',
   padding: '16px',
   width: '25vw',
   height: '50vh',
@@ -19,6 +20,9 @@ Object.assign(output.style, {
   zIndex: '999'
 });
 
+btnClear.textContent = 'Clear';
+
+output.appendChild(btnClear);
 document.body.appendChild(output);
 
 output.dragEnd = (isMobile, dragBind) => {
@@ -26,26 +30,30 @@ output.dragEnd = (isMobile, dragBind) => {
   removeEventListener(moveEvent, dragBind);
   
   output.style.boxShadow = '0px 0px 8px rgba(0, 0, 0, 0.5)';
+
+  output.addEventListener('mousedown', output.initDrag);
+  output.addEventListener('touchstart', output.initDrag);
 };
 
-output.drag = isMobile => {
+output.drag = (isMobile, evt) => {
+  /**
+   * TODO
+   * why offset so jumpy
+   */
   const x = isMobile ? event.changedTouches[0].clientX : event.clientX;
   const y = isMobile ? event.changedTouches[0].clientY : event.clientY;
-  // const offsetX = isMobile ? event.changedTouches[0].layerX : event.layerX;
-  // const offsetY = isMobile ? event.changedTouches[0].layerY : event.layerY;
+  const { layerX, layerY } = evt
   
   Object.assign(output.style, {
-    // top: `${y - offsetY}px`,
-    // left: `${x - offsetX}px`
-    top: `${y}px`,
-    left: `${x}px`
+    top: `${y - layerY}px`,
+    left: `${x - layerX}px`
   });
 };
 
-output.dragStart = isMobile => {
+output.dragStart = (isMobile, evt) => {
   const moveEvent = isMobile ? 'touchmove' : 'mousemove';
   const endEvent = isMobile ? 'touchend' : 'mouseup';
-  const dragBind = output.drag.bind(null, isMobile);
+  const dragBind = output.drag.bind(null, isMobile, evt);
   const dragEndBind = output.dragEnd.bind(null, isMobile, dragBind);
   
   output.style.boxShadow = '0px 0px 8px rgba(128, 0, 255, 0.5)';
@@ -54,29 +62,50 @@ output.dragStart = isMobile => {
   addEventListener(endEvent, dragEndBind);
 };
 
-output.initDrag = isMobile => {
+output.initDrag = () => {
+  const isMobile = event.type === 'touchstart';
   const endEvent = isMobile ? 'touchend' : 'mouseup';
-  const to = setTimeout(output.dragStart.bind(output, isMobile), 500);
+  const to = setTimeout(output.dragStart.bind(null, isMobile, event), 500);
+  console.log('initDrag -> layerX, layerY', event.layerX, event.layerY);
+  
   output.addEventListener(endEvent, () => clearTimeout(to));
+  output.removeEventListener('mousedown', output.initDrag);
+  output.removeEventListener('touchstart', output.initDrag);
 };
 
-output.addEventListener('mousedown', output.initDrag.bind(null, false));
-output.addEventListener('touchstart', output.initDrag.bind(null, true));
+output.addEventListener('mousedown', output.initDrag);
+output.addEventListener('touchstart', output.initDrag);
+
+btnClear.addEventListener('click', () => {
+  event.preventDefault();
+  console.log(event.type);
+  output.innerHTML = '';
+  output.appendChild(btnClear);
+});
 
 window.onerror = function (message, source, lineno, colno, error) {
   const div = document.createElement('div');
   div.style.color = 'crimson';
+  div.style.pointerEvents = 'none';
   div.innerHTML += `${source.split('/').pop()} @ ${lineno}: ${message}`;
   output.appendChild(div);
 };
 
 console.log = function () {
+  const div = document.createElement('div');
   const args = Array.from(arguments);
-  const result = []
+  const result = [];
+  
+  div.style.pointerEvents = 'none';
+  
   for (const arg of args) {
     if (typeof arg === 'object') result.push(JSON.stringify(arg));
     else result.push(arg);
   }
-  output.innerHTML += `<div>Log: ${result.join(' ')}</div>`;
+  
+  div.innerHTML += `Log: ${result.join(' ')}`;
+  
+  output.appendChild(div);
+  
   _log(arguments);
-}
+};
