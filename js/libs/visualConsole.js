f2ffcae17e8504195d193f4a59888671ea8de9e5
Lisa Benmore <lbenmore@gmail.com>
2020-08-25 18:36:31 -0700

    added drag functionality
    janky, but works; need to troubleshoot why offsetting by mouse/touch
    position messes up the whole movement

--- js/libs/visualConsole.js
+++ js/libs/visualConsole.js
@@ -4,8 +4,8 @@ const _log = console.log;
 output.setAttribute('contenteditable', 'true');
 Object.assign(output.style, {
   position: 'fixed',
-  bottom: '8px',
-  right: '8px',
+  top: '8px',
+  left: `${innerWidth - (innerWidth / 4) - 8}px`,
   padding: '16px',
   width: '25vw',
   height: '50vh',
@@ -16,32 +16,52 @@ Object.assign(output.style, {
   opacity: '0.8',
   boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.5)',
   overflow: 'auto',
-  zIndex: '2'
+  zIndex: '999'
 });
 
 document.body.appendChild(output);
 
-output.onkeydown = () => event.preventDefault();
-output.onfocus = () => {
-  Object.assign(output.style, {
-    width: 'calc(100vw - (var(--gutter) * 4))',
-    height: 'calc(100vh - var(--gutter))',
-    backgroundColor: 'white',
-    opacity: '1'
-  });
+output.dragEnd = (isMobile, dragBind) => {
+  const moveEvent = isMobile ? 'touchmove' : 'mousemove';
+  removeEventListener(moveEvent, dragBind);
+  
+  output.style.boxShadow = '0px 0px 8px rgba(0, 0, 0, 0.5)';
 };
-output.onblur = () => {
+
+output.drag = isMobile => {
+  const x = isMobile ? event.changedTouches[0].clientX : event.clientX;
+  const y = isMobile ? event.changedTouches[0].clientY : event.clientY;
+  // const offsetX = isMobile ? event.changedTouches[0].layerX : event.layerX;
+  // const offsetY = isMobile ? event.changedTouches[0].layerY : event.layerY;
+  
   Object.assign(output.style, {
-    width: '25vw',
-    height: '50vh',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    opacity: '0.8'
+    // top: `${y - offsetY}px`,
+    // left: `${x - offsetX}px`
+    top: `${y}px`,
+    left: `${x}px`
   });
 };
 
-$$(output).on('swiperight', () => {
-  output.blur();
-});
+output.dragStart = isMobile => {
+  const moveEvent = isMobile ? 'touchmove' : 'mousemove';
+  const endEvent = isMobile ? 'touchend' : 'mouseup';
+  const dragBind = output.drag.bind(null, isMobile);
+  const dragEndBind = output.dragEnd.bind(null, isMobile, dragBind);
+  
+  output.style.boxShadow = '0px 0px 8px rgba(128, 0, 255, 0.5)';
+  
+  addEventListener(moveEvent, dragBind);
+  addEventListener(endEvent, dragEndBind);
+};
+
+output.initDrag = isMobile => {
+  const endEvent = isMobile ? 'touchend' : 'mouseup';
+  const to = setTimeout(output.dragStart.bind(output, isMobile), 500);
+  output.addEventListener(endEvent, () => clearTimeout(to));
+};
+
+output.addEventListener('mousedown', output.initDrag.bind(null, false));
+output.addEventListener('touchstart', output.initDrag.bind(null, true));
 
 window.onerror = function (message, source, lineno, colno, error) {
   const div = document.createElement('div');
