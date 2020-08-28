984a0fe83842c079856a0d45ff79ad520506aacc
Lisa Benmore <lbenmore@gmail.com>
2020-08-28 11:53:19 -0700

    added files and basic flow process for auth pages

--- js/pages/sign_in.js
+++ js/pages/sign_in.js
@@ -1,3 +1,13 @@
 core.controllers.sign_in = () => {
+  $$('.SignIn .btnSignIn').on('click', () => {
+    core.fns.goToPage('home');
+  });
   
+  $$('.SignIn .btnResetPassword').on('click', () => {
+    core.fns.goToPage('reset_password');
+  });
+  
+  $$('.SignIn .btnRegister').on('click', () => {
+    core.fns.goToPage('register');
+  });
 };
